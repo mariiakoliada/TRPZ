f80302951e8d29f6768df7c63d6dea5c698717a1
Mariia <mashakolyada2005@gmail.com>
2025-11-30 20:23:28 +0200

    Lab6

--- src/ia32/koliada/finance/Main.java
+++ src/ia32/koliada/finance/Main.java
@@ -1,54 +1,24 @@
 package ia32.koliada.finance;
 
-import ia32.koliada.finance.builder.SimpleTransactionBuilder;
-import ia32.koliada.finance.builder.TransactionBuilder;
-import ia32.koliada.finance.builder.TransactionDirector;
-import ia32.koliada.finance.entity.Account;
+import ia32.koliada.finance.decorator.*;
 import ia32.koliada.finance.entity.Transaction;
-import ia32.koliada.finance.repository.AccountRepository;
-import ia32.koliada.finance.repository.TransactionRepository;
-import ia32.koliada.finance.service.FinanceService;
-import ia32.koliada.finance.ui.MainWindow;
-
-import javax.swing.*;
 import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        initMockData();
+        Transaction tx = new Transaction(1L, 1L, 1L, new BigDecimal("-500.00"), "Подарунок");
 
-        System.out.println("\n===BUILDER==");
+        System.out.println("\n===DECORATOR===");
 
-        TransactionBuilder builder = new SimpleTransactionBuilder();
-        Transaction t1 = builder.setAccountId(1L)
-                .setAmount(new BigDecimal("-500"))
-                .setDescription("Супермаркет")
-                .setType("EXPENSE")
-                .setCurrentDate()
-                .build();
+        TransactionPrinter simple = new SimpleTransactionPrinter();
+        System.out.println("Simple: " + simple.print(tx));
 
-        System.out.println("Створено вручну: " + t1);
+        TransactionPrinter html = new HtmlPrinter(simple);
+        System.out.println("HTML:   " + html.print(tx));
 
-        TransactionDirector director = new TransactionDirector();
-        Transaction coffee = director.constructCoffeeTransaction(new SimpleTransactionBuilder(), 1L);
+        TransactionPrinter secureHtml = new SecurePrinter(new HtmlPrinter(new SimpleTransactionPrinter()));
+        System.out.println("Combo:  " + secureHtml.print(tx));
 
-        System.out.println("Створено Директором: " + coffee);
-
-        TransactionRepository txRepo = new TransactionRepository();
-        txRepo.save(t1);
-        txRepo.save(coffee);
-
-        System.out.println("=====================================\n");
-
-        SwingUtilities.invokeLater(() -> {
-            FinanceService service = new FinanceService();
-            MainWindow window = new MainWindow(service);
-            window.setVisible(true);
-        });
-    }
-
-    private static void initMockData() {
-        AccountRepository repo = new AccountRepository();
-        repo.save(new Account(1L, 100L, "Monobank", new BigDecimal("5000.00")));
+        System.out.println("====================================\n");
     }
 }
